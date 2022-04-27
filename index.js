cbed20d689cc84ba6ac8658048807aef0b29047b
Naftali Kulik <nkulik1994@gmail.com>
2022-04-27 14:17:41 -0400

    commit tree 75e8f4e780dbbb7ad29fd7550efe482b9e334101

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(array) {
+    function checkResult(element, index, array) {
+        return Object.values(element)[1] === "W"
+    }
+    const rightObject = array.find(checkResult)
+    if (typeof rightObject === 'object') {
+        return Object.values(rightObject)[0]
+    }
+}
